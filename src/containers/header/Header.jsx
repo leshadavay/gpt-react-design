0f83d30036c6f157592ce9869010e0ab6dd38ab6
lesham <leshadavay@gmail.com>
2022-03-26 15:03:37 +0500

    features, possibility section

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -4,7 +4,7 @@ import people from "../../assets/people.png";
 import ai from "../../assets/ai.png";
 const Header = () => {
   return (
-    <div className="gpt3__header section__padding">
+    <div className="gpt3__header section__padding" id="home">
       <div className="gpt3__header-content">
         <h1 className="gradient__text">
           Let’s Build Something amazing with GPT-3 OpenAI
